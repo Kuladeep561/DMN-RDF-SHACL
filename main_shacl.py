91da643982eb518d4343aacc1085e5c80537d11e
Kuladeep561 <kuladeep561@gmail.com>
2023-05-11 17:01:08 +0200

    updated relative paths

--- main_shacl.py
+++ main_shacl.py
@@ -7,7 +7,7 @@ from pylib.create_SHACL_rule import *
 
 ### **parse DMN table as xml and create DataFrame**###
 
-dmn_filepath = r"C:\Users\kuk\Downloads\geometric-inspections.dmn"
+dmn_filepath = r"./graphs and DMN/geometric-inspections.dmn"
 
 
 parser = DMNParser(dmn_filepath)
@@ -21,9 +21,9 @@ df_inputs, df_outputs = parser.dmn_as_dataframe(inputs, outputs)  # Convert into
 
 if (hit_policy.lower() == "collect"):
 
-    ontology = Graph().parse("./graphs/DMN-RDF-Dicon-OCQA-Tbox.ttl",
+    ontology = Graph().parse("./graphs and DMN/DMN-RDF-Dicon-OCQA-Tbox.ttl",
                              format="ttl")
-    example_building = Graph().parse("./graphs/Duplex_A_20110505_LBD.ttl",
+    example_building = Graph().parse("./graphs and DMN/Duplex_A_20110505_LBD.ttl",
                                      format="ttl")
     
     _combined = ontology + example_building
@@ -36,10 +36,10 @@ if (hit_policy.lower() == "collect"):
         output_row = df_outputs.iloc[index]
         
         shacl_rule = generate_shacl(in_row, output_row, ns, classes, relationships)
-        with open(f'shacl_rule_{index}.ttl', 'w') as f:
+        with open(f'./inferred rules/shacl_rule_{index}.ttl', 'w') as f:
             f.write(shacl_rule)
         
-        rules_graph = Graph().parse(f"shacl_rule_{index}.ttl",
+        rules_graph = Graph().parse(f"./inferred rules/shacl_rule_{index}.ttl",
                                 format="ttl")  # Load the SHACL rules graph
 
         # Validate the combined graph and apply the rules
@@ -50,7 +50,7 @@ if (hit_policy.lower() == "collect"):
 
     new_graph = _combined + inferred_graph
     new_graph.serialize(
-        destination="Inferred_geometry_inspections.ttl", format="ttl")
+        destination="./inferred graphs/Inferred_geometry_inspections.ttl", format="ttl")
 
 else:
     raise ValueError(f"The hit policy is: {hit_policy}, it is unsupported. The script supports only 'COLLECT'")
